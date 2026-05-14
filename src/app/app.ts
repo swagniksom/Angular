c41e787c64d05e7807d02a00fd0f51e8b23bb546
SwagnikSom <swagnik.som@dharbor.com>
2026-05-14 11:13:48 +0530

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {header} from './header';
+import { UserComponent } from './user-component/user-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, header, UserComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('app1');
---
+++ src/app/user-component/user-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-component',
+  imports: [],
+  templateUrl: './user-component.html',
+  styleUrl: './user-component.css',
+})
+export class UserComponent {}
