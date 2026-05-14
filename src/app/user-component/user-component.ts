43d946ae14631bd169cce72be99e415181f250df
SwagnikSom <swagnik.som@dharbor.com>
2026-05-14 20:24:58 +0530

    fiest commit

--- src/app/user-component/user-component.ts
+++ src/app/user-component/user-component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
-
+import {DUMMY_USERS} from '../dummy-user';
+const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length);
 @Component({
   selector: 'app-user-component',
   imports: [],
   templateUrl: './user-component.html',
   styleUrl: './user-component.css',
 })
-export class UserComponent {}
+export class UserComponent {
+   selectUser=DUMMY_USERS[randomIndex]
+  protected readonly name = name;
+   get imagePath(){
+     return 'assets/users/'+this.selectUser.avatar;
+   }
+   onSelectUser(){
+     const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length);
+     this.selectUser=DUMMY_USERS[randomIndex]
+     console.log("user...")
+   }
+}
