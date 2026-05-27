5d71aef7decc307f787c5546e7871f036fa8898d
swagnik <swagniksom3@gmail.com>
2026-05-27 23:21:35 +0530

    commit two

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,27 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {header} from './header';
 import { UserComponent } from './user-component/user-component';
+import {DUMMY_USERS} from '../assets/DUMMY-USER';
+import {Tasks} from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, header, UserComponent],
+  imports: [RouterOutlet, header, UserComponent,Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
+  // selectUsersId?:string;---for checking the if else
+  //   selectUsersId='u1;--actual...
+
+  selectUsersId?:string;
+  users=DUMMY_USERS;
+  onselectUsers(id:string){
+    console.log("The user id is"+id);
+    this.selectUsersId=id;
+  }
+  get selectteduser(){
+    return this.users.find((user)=>user.id===this.selectUsersId);
+  }
   protected readonly title = signal('app1');
 }
--- src/app/user-component/user-component.ts
+++ src/app/user-component/user-component.ts
@@ -1,21 +1,60 @@
-import { Component } from '@angular/core';
-import {DUMMY_USERS} from '../dummy-user';
+import {Component, computed, EventEmitter, input, Input, Output, signal} from '@angular/core';
+import {DUMMY_USERS} from '../../assets/DUMMY-USER';
+// import {DUMMY_USERS}
 const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length);
+// we can use the object also
+// type User={
+//   id:string,
+//   name:string,
+//   avatar:string
+// }
+// and also we can use the interface
+interface User{
+  id:string,
+   name:string,
+   avatar:string
+}
 @Component({
   selector: 'app-user-component',
   imports: [],
   templateUrl: './user-component.html',
   styleUrl: './user-component.css',
 })
+// NOW THIS PROPEERTY  SI SETTABLEFROM OUTSDE
 export class UserComponent {
-   selectUser=DUMMY_USERS[randomIndex]
-  protected readonly name = name;
+  //    selectUser= signal(DUMMY_USERS[randomIndex])
+  // protected readonly name = name;
    get imagePath(){
-     return 'assets/users/'+this.selectUser.avatar;
+     return 'assets/users/'+this.user.avatar;
    }
+  //  // using signals
+  // imagePath=computed(()=>
+  //   'assets/users/' + this.avatar()
+  // );
+  // @Input({required:true})id!:string
+  // @Input({required:true}) avatar!:String;
+  // @Input({required:true})name!:string;
+  // @Input({required:true})user!:{
+  //   id:string,
+  //   name:string,
+  //   avatar:string
+  // }
+  // also we can write  like
+ @Input({required:true})user!:User
+  @Output()select=new EventEmitter();
+  //  --------------------
+  // For AngularSignals
+  // avatar=input.required<string>()
+  // name=input.required<string>()
+  // --------------------
    onSelectUser(){
-     const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length);
-     this.selectUser=DUMMY_USERS[randomIndex]
-     console.log("user...")
+     // const randomIndex=Math.floor(Math.random()*DUMMY_USERS.length);
+     // // this.selectUser=DUMMY_USERS[randomIndex]
+     // // using signal
+     // this.selectUser.set(DUMMY_USERS[randomIndex]);
+     // console.log("user...")
+     this.select.emit(this.user.id)
    }
+  // ----------------------------------
+
 }
